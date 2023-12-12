a6f033088204db3ef7fab1299b8add491ff2d3fd
notify <notify-ctrl@qq.com>
2023-12-12 20:14:36 +0800

    to 0.3.14: useActive

--- mobile_sp.lua
+++ mobile_sp.lua
@@ -4676,7 +4676,7 @@ local luanqun = fk.CreateActiveSkill{
       reason = self.name,
     }
     for _, p in ipairs(targets) do
-      p.request_data = json.encode({"choose_cards_skill", "#luanqun-card", true, json.encode(extraData)})
+      p.request_data = json.encode({"choose_cards_skill", "#luanqun-card", true, extraData})
     end
     room:notifyMoveFocus(room.alive_players, self.name)
     room:doBroadcastRequest("AskForUseActiveSkill", targets)

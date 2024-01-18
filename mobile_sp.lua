a75b6b224c123975c8ef80595764fff9050cde30
Ho-spair <linyuy@163.com>
2024-01-18 23:07:38 +0800

    周群死亡清除场上“天算”标记，后续改为标记认主

--- mobile_sp.lua
+++ mobile_sp.lua
@@ -1599,7 +1599,7 @@ local tiansuanTrig = fk.CreateTriggerSkill{
     end
   end,
 
-  refresh_events = { fk.TurnStart },
+  refresh_events = { fk.TurnStart, fk.Death },
   can_refresh = function(self, event, target, player, data)
     return target == player and player:getMark("tiansuan") ~= 0
   end,

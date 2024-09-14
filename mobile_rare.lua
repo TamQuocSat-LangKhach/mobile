d0c63b728037ac70db5ee0edee797d20c9c7338d
Ho-spair <linyuy@163.com>
2024-09-14 09:30:08 +0800

    修复改版忍戒

--- mobile_rare.lua
+++ mobile_rare.lua
@@ -2852,13 +2852,16 @@ local mobileRenjie = fk.CreateTriggerSkill{
       not player:hasSkill(self) or
       not data.eventData or
       data.eventData.from == player.id or
-      (data.result and data.result.from == player.id) or
       player:usedSkillTimes(self.name, Player.HistoryRound) > 3
     then
       return false
     end
 
-    return event == fk.AfterAskForNullification or target == player
+    if event == fk.AfterAskForCardResponse then
+      return target == player and not data.result
+    end
+
+    return not (data.result and data.result.from == player.id) and (event == fk.AfterAskForNullification or target == player)
   end,
   on_use = function(self, event, target, player, data)
     player.room:addPlayerMark(player, "@mobile__renjie_ren")
@@ -2866,7 +2869,7 @@ local mobileRenjie = fk.CreateTriggerSkill{
 }
 Fk:loadTranslationTable{
   ["mobile__renjie"] = "忍戒",
-  [":mobile__renjie"] = "锁定技，每轮限四次，当你需要响应一张牌时，若你不为使用者且未响应，你获得一枚“忍”标记。",
+  [":mobile__renjie"] = "锁定技，每轮限四次，当你需要使用或打出牌以响应一张牌时，若你不为使用者且未响应，你获得一枚“忍”标记。",
   ["@mobile__renjie_ren"] = "忍",
   ["$mobile__renjie1"] = "朝中大小事宜，自有大将军定夺。",
   ["$mobile__renjie2"] = "朝论政事，老夫唯大将军马首是瞻。",

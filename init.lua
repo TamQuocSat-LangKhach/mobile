65fb83cbc23ed73b54cb21d47afdce80fdc92d09
seven <786852516@qq.com>
2024-09-30 11:51:42 +0800

    修复界袁术|new标袁术|new新分包

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ local sincerity = require "packages/mobile/sincerity"
 local benevolence = require "packages/mobile/benevolence"
 local courage = require "packages/mobile/courage"
 local strictness = require "packages/mobile/strictness"
+local mobile_re = require "packages/mobile/mobile_re"
 local mShzlEx = require "packages/mobile/m_shzl_ex"
 local mYjEx = require "packages/mobile/m_yj_ex"
 local mobileDerived = require "packages/mobile/mobile_derived"
@@ -23,6 +24,7 @@ return {
   benevolence,
   courage,
   strictness,
+  mobile_re,
   mShzlEx,
   mYjEx,
   mobileDerived,

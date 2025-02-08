2983b64f09fc4c78bfdefee3db686e694c16cb43
xxyheaven <1433191064@qq.com>
2025-02-09 00:20:31 +0800

    new 娄圭、清河公主

--- mobile_sp2.lua
+++ mobile_sp2.lua
@@ -2316,4 +2316,454 @@ Fk:loadTranslationTable{
   ["#yingjia-choose"] = "迎驾：弃置一张手牌，令一名角色获得一个额外回合",
 }
 
+local shuffleCardtoDrawPile = function (player, cards, skillName, proposer)
+  proposer = proposer or player.id
+  local room = player.room
+  local x = #cards
+  table.shuffle(cards)
+  local positions = {}
+  local y = #room.draw_pile
+  for _ = 1, x, 1 do
+    table.insert(positions, math.random(y+1))
+  end
+  table.sort(positions, function (a, b)
+    return a > b
+  end)
+  local moveInfos = {}
+  for i = 1, x, 1 do
+    table.insert(moveInfos, {
+      ids = {cards[i]},
+      from = player.id,
+      toArea = Card.DrawPile,
+      moveReason = fk.ReasonJustMove,
+      skillName = skillName,
+      drawPilePosition = positions[i],
+      proposer = proposer,
+    })
+  end
+  room:moveCards(table.unpack(moveInfos))
+end
+
+local guansha = fk.CreateTriggerSkill{
+  name = "guansha",
+  frequency = Skill.Limited,
+  anim_type = "defensive",
+  events = {fk.EventPhaseEnd},
+  can_trigger = function(self, event, target, player, data)
+    return player == target and player.phase == Player.Play and
+      player:hasSkill(self) and player:usedSkillTimes(self.name, Player.HistoryGame) == 0
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local cards = player:getCardIds("he")
+    local x = #cards
+    if x == 0 then return false end
+    shuffleCardtoDrawPile(player, cards, self.name)
+    if player.dead then return false end
+    cards = room:getCardsFromPileByRule(".|.|.|.|.|basic", x)
+    if #cards > 0 then
+      local names = {}
+      for _, id in ipairs(cards) do
+        table.insertIfNeed(names, Fk:getCardById(id).trueName)
+      end
+      room:obtainCard(player, cards, false, fk.ReasonJustMove, player.id, self.name)
+      if not player.dead then
+        room:addPlayerMark(player, MarkEnum.AddMaxCardsInTurn, #names)
+      end
+    end
+  end,
+}
+
+Fk:loadTranslationTable{
+  ["guansha"] = "灌沙",
+  [":guansha"] = "限定技，出牌阶段结束时，你可以将所有牌替换为牌堆中等量的基本牌，"..
+    "然后你于此回合内手牌上限+X（X为你以此法得到的牌的牌名数）。",
+
+  ["$guansha1"] = "",
+  ["$guansha2"] = "",
+}
+
+local jiyu = fk.CreateActiveSkill{
+  name = "jiyul",
+  anim_type = "drawcard",
+  prompt = "#jiyul-active",
+  max_phase_use_time = 1,
+  card_num = 1,
+  target_num = 0,
+  card_filter = function(self, to_select, selected, player)
+    return #selected == 0 and Fk:currentRoom():getCardArea(to_select) == Card.PlayerHand and
+      not player:prohibitDiscard(Fk:getCardById(to_select))
+  end,
+  on_use = function(self, room, effect)
+    local player = room:getPlayerById(effect.from)
+    local cardType = Fk:getCardById(effect.cards[1]):getTypeString()
+    room:throwCard(effect.cards, self.name, player, player)
+    if player.dead then return end
+    local types = { "basic", "trick", "equip" }
+    table.removeOne(types, cardType)
+    local cardMap = {}
+    cardMap["basic"] = {}
+    cardMap["trick"] = {}
+    cardMap["equip"] = {}
+    for _, id in ipairs(room.draw_pile) do
+      table.insert(cardMap[Fk:getCardById(id):getTypeString()], id)
+    end
+    local toGet = {}
+    for _, typeName in ipairs(types) do
+      if #cardMap[typeName] > 0 then
+        table.insert(toGet, table.random(cardMap[typeName]))
+      end
+    end
+    if #toGet > 0 then
+      room:obtainCard(player, toGet, false, fk.ReasonJustMove, player.id, self.name, "@@jiyul-phase")
+    end
+  end,
+
+  on_lose = function (self, player)
+    U.clearHandMark(player, "@@jiyul-phase")
+  end
+}
+
+local jiyuRefresh = fk.CreateTriggerSkill{
+  name = "#jiyul_refresh",
+
+  refresh_events = {fk.AfterCardsMove},
+  can_refresh = function(self, event, target, player, data)
+    return player:hasSkill(jiyu, true)
+  end,
+  on_refresh = function(self, event, target, player, data)
+    local room = player.room
+    local loseByUse = false
+    local loseByOtherReason = false
+    local card
+    for _, move in ipairs(data) do
+      if move.from == player.id then
+        for _, info in ipairs(move.moveInfo) do
+          card = Fk:getCardById(info.cardId)
+          if card:getMark("@@jiyul-phase") ~= 0 then
+            room:setCardMark(card, "@@jiyul-phase", 0)
+            if move.moveReason == fk.ReasonUse then
+              loseByUse = true
+            else
+              loseByOtherReason = true
+            end
+          end
+        end
+      end
+    end
+    if loseByOtherReason then
+      U.clearHandMark(player, "@@jiyul-phase")
+    elseif loseByUse and table.every(player:getCardIds(Player.Hand), function (id)
+      return Fk:getCardById(id):getMark("@@jiyul-phase") == 0
+    end) then
+      player:setSkillUseHistory("jiyul", 0, Player.HistoryPhase)
+    end
+  end,
+}
+
+jiyu:addRelatedSkill(jiyuRefresh)
+
+Fk:loadTranslationTable{
+  ["jiyul"] = "急御",
+  [":jiyul"] = "出牌阶段限一次，你可以弃置一张手牌，从牌堆随机获得此牌类别以外的牌各一张，若你使用了这些牌，此技能视为未发动过。",
+
+  ["#jiyul-active"] = "发动 急御，弃置一张手牌，从牌堆随机获得此牌类别以外的牌各一张",
+  ["@@jiyul-phase"] = "急御",
+
+  ["$jiyul1"] = "",
+  ["$jiyul2"] = "",
+}
+
+local lougui = General(extension, "mobile__lougui", "wei", 3)
+lougui:addSkill(guansha)
+lougui:addSkill(jiyu)
+
+Fk:loadTranslationTable{
+  ["mobile__lougui"] = "娄圭",
+  ["#mobile__lougui"] = "一日之寒",
+  --["mobile__lougui:lougui"] = "",
+  ["~mobile__qinghegongzhu"] = "",
+}
+
+local zengou = fk.CreateActiveSkill{
+  name = "mobile__zengou",
+  anim_type = "control",
+  prompt = "#mobile__zengou-active",
+  max_phase_use_time = 1,
+  card_num = 0,
+  target_num = 1,
+  card_filter = Util.FalseFunc,
+  target_filter = function(self, to_select, selected, selected_cards, card, extra_data, player)
+    return #selected == 0 and to_select ~= player.id and not Fk:currentRoom():getPlayerById(to_select):isKongcheng() and
+      not table.contains(player:getTableMark("mobile__zengou_prohibit"), to_select)
+  end,
+  on_use = function(self, room, effect)
+    local player = room:getPlayerById(effect.from)
+    local target = room:getPlayerById(effect.tos[1])
+    local cids = target:getCardIds(Player.Hand)
+    local choice = U.askforViewCardsAndChoice(player, cids, {"mobile__zengou_use", "mobile__zengou_exchange"},
+      self.name, "#mobile__zengou-choose::" .. target.id)
+    local card
+    local cardName
+    if choice == "mobile__zengou_use" then
+      local cards = U.getUniversalCards(room, "b", false)
+      local toUse = table.filter(cards, function(id)
+        cardName = Fk:getCardById(id).trueName
+        return table.every(cids, function(id2)
+          return cardName ~= Fk:getCardById(id2).trueName
+        end)
+      end)
+      while #toUse > 0 do
+        local use = U.askForUseRealCard(room, player, toUse, nil, self.name,
+          "#mobile__zengou-use", {expand_pile = toUse, bypass_times = true}, true, false)
+        if use then
+          room:addPlayerMark(player, "@mobile__zengou-round")
+          card = Fk:cloneCard(use.card.name)
+          card.skillName = self.name
+          room:useCard{
+            card = card,
+            from = player.id,
+            tos = use.tos,
+            extraUse = true,
+          }
+          if player.dead then return end
+          cardName = card.trueName
+          toUse = table.filter(toUse, function(id)
+            return Fk:getCardById(id).trueName ~= cardName
+          end)
+        else
+          break
+        end
+      end
+    else
+      local cardMap = {}
+      for _, id in ipairs(player:getCardIds(Player.Hand)) do
+        card = Fk:getCardById(id)
+        cardName = card.trueName
+        cardMap[cardName] = cardMap[cardName] or {}
+        table.insert(cardMap[cardName], id)
+      end
+      local toPut = {}
+      local cardNames = {}
+      for _, id in ipairs(cids) do
+        card = Fk:getCardById(id)
+        cardName = card.trueName
+        if cardMap[cardName] then
+          table.insert(cardNames, cardName)
+          table.insertTable(toPut, cardMap[cardName])
+          cardMap[cardName] = nil
+        end
+      end
+      local x = #toPut
+      if x > 0 then
+        shuffleCardtoDrawPile(player, toPut, self.name)
+        if not player.dead then
+          toPut = room:getCardsFromPileByRule("slash", x)
+          if #toPut > 0 then
+            room:obtainCard(player, toPut, false, fk.ReasonJustMove, player.id, self.name, "@@mobile__zengou-inhand")
+          end
+        end
+        toPut = table.filter(target:getCardIds(Player.Hand), function (id)
+          return table.contains(cardNames, Fk:getCardById(id).trueName)
+        end)
+        x = #toPut
+        if x > 0 then
+          shuffleCardtoDrawPile(target, toPut, self.name, player.id)
+          if not target.dead then
+            toPut = room:getCardsFromPileByRule("slash", x)
+            if #toPut > 0 then
+              room:obtainCard(target, toPut, false, fk.ReasonJustMove, player.id, self.name, "@@mobile__zengou-inhand")
+            end
+          end
+        end
+      end
+    end
+    if player:hasSkill(self, true) and not target.dead then
+      local cards = U.getUniversalCards(room, "b", true)
+      if #cards > 0 then
+        local id = room:askForCardChosen(
+          player,
+          target,
+          {
+            card_data = {
+              { "basic", cards }
+            }
+          },
+          self.name,
+          "#mobile__zengou-bname::" .. target.id
+        )
+        U.setPrivateMark(target, "mobile__zengou_wu", { Fk:getCardById(id).trueName }, { player.id })
+      end
+    end
+  end,
+
+  on_lose = function (self, player)
+    local room = player.room
+    room:setPlayerMark(player, "@mobile__zengou-round", 0)
+    room:setPlayerMark(player, "mobile__zengou_prohibit", 0)
+  end
+}
+
+local zengouTrigger = fk.CreateTriggerSkill{
+  name = "#mobile__zengou_trigger",
+  anim_type = "negative",
+  events = {fk.CardUseFinished},
+  can_trigger = function(self, event, target, player, data)
+    if player.id ~= data.from or not table.contains(U.getPrivateMark(player, "mobile__zengou_wu"), data.card.trueName) then
+      return false
+    end
+    local room = player.room
+    local logic = room.logic
+    local use_event = logic:getCurrentEvent()
+    local mark = player:getMark("mobile__zengou-turn")
+    if mark == 0 then
+      logic:getEventsOfScope(GameEvent.UseCard, 1, function (e)
+        local last_use = e.data[1]
+        if last_use.from == player.id then
+          mark = e.id
+          room:setPlayerMark(player, "mobile__zengou-turn", mark)
+          return true
+        end
+        return false
+      end, Player.HistoryTurn)
+    end
+    return mark == use_event.id
+  end,
+  on_cost = Util.TrueFunc,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    room:setPlayerMark(player, "@[private]mobile__zengou_wu", 0)
+    room:loseHp(player, 1, self.name)
+  end,
+}
+
+local zengouRefresh = fk.CreateTriggerSkill{
+  name = "#mobile__zengou_refresh",
+
+  refresh_events = {fk.AfterTurnEnd},
+  can_refresh = function(self, event, target, player, data)
+    return player == target
+  end,
+  on_refresh = function(self, event, target, player, data)
+    U.clearHandMark(player, "@@mobile__zengou-inhand")
+  end,
+}
+
+local zengouMaxCards = fk.CreateMaxCardsSkill{
+  name = "#mobile__zengou_maxcards",
+  exclude_from = function(self, player, card)
+    return card:getMark("@@mobile__zengou-inhand") > 0
+  end,
+}
+
+zengou:addRelatedSkill(zengouTrigger)
+zengou:addRelatedSkill(zengouRefresh)
+zengou:addRelatedSkill(zengouMaxCards)
+
+Fk:loadTranslationTable{
+  ["mobile__zengou"] = "谮构",
+  [":mobile__zengou"] = "出牌阶段限一次，你可以观看一名角色的所有手牌，选择："..
+    "1.依次可以视为使用其手牌区里没有的牌名的基本牌各一张（不计入次数且无次数限制）；"..
+    "2.你与其依次将手牌区里的共有牌名的牌替换为牌堆中等量的【杀】（以此法得到的【杀】直到各自的回合结束之前不计入手牌上限）。"..
+    "然后其获得一个“诬”标记并记录一个你指定的基本牌名。"..
+    "拥有此标记的角色每回合使用的第一张牌结算后，若与记录的牌名相同，其移除此标记并失去1点体力。",
+
+  ["#mobile__zengou-active"] = "发动 谮构，选择一名角色，观看其所有手牌",
+  ["#mobile__zengou-choose"] = "谮构：观看%dest的手牌并选择一项",
+  ["mobile__zengou_use"] = "视为使用基本牌",
+  ["mobile__zengou_exchange"] = "将牌替换为【杀】",
+  ["#mobile__zengou-use"] = "谮构：你可以依次使用不同牌名的基本牌各一张（不计入次数且无次数限制）",
+  ["#mobile__zengou-bname"] = "谮构：为%dest的“诬”标记记录一种基本牌的名称",
+  ["#mobile__zengou_trigger"] = "谮构",
+  ["@mobile__zengou-round"] = "谮构",
+
+  ["@@mobile__zengou-inhand"] = "谮构",
+  ["@[private]mobile__zengou_wu"] = "诬",
+
+  ["$mobile__zengou1"] = "",
+  ["$mobile__zengou2"] = "",
+}
+
+local feili = fk.CreateTriggerSkill{
+  name = "feili",
+  anim_type = "defensive",
+  events = {fk.DamageInflicted},
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(self) and player:hasSkill("mobile__zengou", true) and
+      (#player:getCardIds("he") >= math.max(1, player:getMark("@mobile__zengou-round")) or
+      (data.from and data.from:getMark("@[private]mobile__zengou_wu") ~= 0))
+  end,
+  on_cost = function(self, event, target, player, data)
+    local x = math.max(1, player:getMark("@mobile__zengou-round"))
+    if data.from and data.from:getMark("@[private]mobile__zengou_wu") ~= 0 then
+      local cards, choice = U.askForCardByMultiPatterns(
+        player,
+        {
+          { ".", 0, 0, "feili_removemark::" .. data.from.id },
+          { ".", x, x, "feili_discard:::" .. tostring(x) }
+        },
+        self.name,
+        true,
+        "#feili-invoke",
+        {
+          discard_skill = true
+        }
+      )
+      if choice == "" then return false end
+      if #cards > 0 then
+        self.cost_data = { cards = cards }
+        return true
+      else
+        self.cost_data = { tos = { data.from.id }, cards = cards }
+        return true
+      end
+    else
+      local cards = player.room:askForDiscard(player, x, x, true, self.name, true, ".", "#feili-discard:::" .. tostring(x), true)
+      if #cards > 0 then
+        self.cost_data = { cards = cards }
+        return true
+      end
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local cards = self.cost_data.cards
+    if #cards > 0 then
+      room:throwCard(cards, self.name, player)
+    else
+      room:setPlayerMark(data.from, "@[private]mobile__zengou_wu", 0)
+      room:drawCards(player, 2, self.name)
+      if player:hasSkill("mobile__zengou", true) then
+        room:addTableMark(player, "mobile__zengou_prohibit", data.from.id)
+      end
+    end
+    return true
+  end,
+}
+
+Fk:loadTranslationTable{
+  ["feili"] = "诽离",
+  [":feili"] = "当你受到伤害后，若你拥有〖谮构〗，你可以弃置X张牌来防止此伤害（X为你于此轮内因〖谮构〗而使用过的牌数且至少为1），"..
+    "若来源拥有“诬”标记，你可以改为移除此标记来防止此伤害，然后你摸两张牌且本局游戏不能对其发动〖谮构〗。",
+
+  ["#feili-invoke"] = "是否发动 诽离，弃置“诬”标记或牌来防止伤害",
+  ["#feili-discard"] = "是否发动 诽离，弃置%arg张牌来防止伤害",
+  ["feili_removemark"] = "移除%dest的“诬”标记",
+  ["feili_discard"] = "弃置%arg张牌",
+
+  ["$feili1"] = "",
+  ["$feili2"] = "",
+}
+
+local qinghegongzhu = General(extension, "mobile__qinghegongzhu", "wei", 3, 3, General.Female)
+qinghegongzhu:addSkill(zengou)
+qinghegongzhu:addSkill(feili)
+
+Fk:loadTranslationTable{
+  ["mobile__qinghegongzhu"] = "清河公主",
+  ["#mobile__qinghegongzhu"] = "蛊虿之谗",
+  --["illustrator:mobile__qinghegongzhu"] = "君桓文化",
+  ["~mobile__qinghegongzhu"] = "",
+}
+
 return extension
